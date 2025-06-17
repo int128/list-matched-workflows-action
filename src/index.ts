dec5a3f77fdbf7f16d6abf208d613ff1912b3589
Hidetake Iwata <int128@gmail.com>
2025-06-17 11:53:12 +0900

    Improve

--- src/index.ts
+++ src/index.ts
@@ -3,13 +3,14 @@ import { run } from './run.js'
 import { getContext, getOctokit } from './github.js'
 
 try {
-  await run(
+  const outputs = await run(
     {
       workflows: core.getInput('workflows', { required: true }),
     },
     getOctokit(),
     await getContext(),
   )
+  core.setOutput('matched-workflows-json', outputs.matchedWorkflows)
 } catch (e) {
   core.setFailed(e instanceof Error ? e : String(e))
   console.error(e)
--- src/run.ts
+++ src/run.ts
@@ -17,10 +17,33 @@ type Inputs = {
   workflows: string
 }
 
-export const run = async (inputs: Inputs, octokit: Octokit, context: Context): Promise<void> => {
-  assert('pull_request' in context.payload, 'This action must be run on a pull_request event')
+type Outputs = {
+  matchedWorkflows: WorkflowFile[]
+}
 
-  core.info(`Fetching the list of changed files in #${context.payload.pull_request.number}`)
+type WorkflowFile = {
+  filename: string
+  workflow: Workflow
+}
+
+export const run = async (inputs: Inputs, octokit: Octokit, context: Context): Promise<Outputs> => {
+  assert('pull_request' in context.payload, 'This action must be run on a pull_request event')
+  core.info(`pull_request.type: ${context.payload.action}`)
+  core.info(`pull_request.branch: ${context.payload.pull_request.base.ref}`)
+
+  const workflowFiles: WorkflowFile[] = []
+  const workflowGlob = await glob.create(inputs.workflows)
+  const workflowFilenames = await workflowGlob.glob()
+  core.startGroup(`Parsing ${workflowFilenames.length} workflows`)
+  for (const filename of workflowFilenames) {
+    core.info(`Parsing ${filename}`)
+    const workflow: unknown = yaml.load(await fs.readFile(filename, 'utf8'))
+    assertIsWorkflow(workflow)
+    workflowFiles.push({ filename, workflow })
+  }
+  core.endGroup()
+
+  core.info(`Fetching the list of changed files in ${context.payload.pull_request.html_url}`)
   const listFiles = await octokit.paginate(octokit.rest.pulls.listFiles, {
     owner: context.repo.owner,
     repo: context.repo.repo,
@@ -28,32 +51,32 @@ export const run = async (inputs: Inputs, octokit: Octokit, context: Context): P
     per_page: 100,
   })
   const changedFiles = listFiles.map((file) => file.filename)
-  core.info(`Found ${changedFiles.length} changed files in #${context.payload.pull_request.number}`)
-
-  const workflows: Workflow[] = []
-  const workflowGlob = await glob.create(inputs.workflows)
-  const workflowFilenames = await workflowGlob.glob()
-  for (const workflowFilename of workflowFilenames) {
-    core.info(`Parsing ${workflowFilename}`)
-    const workflow: unknown = yaml.load(await fs.readFile(workflowFilename, 'utf8'))
-    assertIsWorkflow(workflow)
-    workflows.push(workflow)
+  core.startGroup(`Found ${changedFiles.length} changed files`)
+  for (const file of changedFiles) {
+    core.info(file)
   }
+  core.endGroup()
 
-  core.info(`Filtering ${workflows.length} workflows based on the event`)
-  core.info(`pull_request.type: ${context.payload.action}`)
-  core.info(`pull_request.branch: ${context.payload.pull_request.base.ref}`)
+  core.startGroup(`Filtering ${workflowFiles.length} workflows based on the event`)
   const matchedWorkflows = []
-  for (const workflow of workflows) {
-    if (!matchPullRequestType(workflow, context.payload.action)) {
+  for (const workflowFile of workflowFiles) {
+    if (!matchPullRequestType(workflowFile.workflow, context.payload.action)) {
+      core.info(`${workflowFile.filename}: type did not match`)
       continue
     }
-    if (!matchPullRequestBranch(workflow, context.payload.pull_request.base.ref)) {
+    if (!matchPullRequestBranch(workflowFile.workflow, context.payload.pull_request.base.ref)) {
+      core.info(`${workflowFile.filename}: branch did not match`)
       continue
     }
-    if (!matchPullRequestPaths(workflow, changedFiles)) {
+    if (!matchPullRequestPaths(workflowFile.workflow, changedFiles)) {
+      core.info(`${workflowFile.filename}: changed files did not match`)
       continue
     }
-    matchedWorkflows.push(workflow)
+    core.info(`${workflowFile.filename}: matched`)
+    matchedWorkflows.push(workflowFile)
   }
+  core.endGroup()
+
+  core.info(`Matched ${matchedWorkflows.length} workflows`)
+  return { matchedWorkflows }
 }
